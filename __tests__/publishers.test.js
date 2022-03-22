0c7ea02d6384fb8540eb8fb09f9500d0a90f5436
Phoenix Nicholson <phoenix.nicholson2008@gmail.com>
2022-03-22 11:59:48 -0700

    wrote passing test for findAllPublisher

--- __tests__/publishers.test.js
+++ __tests__/publishers.test.js
@@ -2,24 +2,37 @@ const setup = require('../data/setup');
 const app = require('../lib/app');
 const request = require('supertest');
 const pool = require('../lib/utils/pool');
+const Publisher = require('../lib/models/Publisher');
 
 describe('sql-bookstore routes', () => {
-    beforeEach(() => {
-      return setup(pool);
-    });
-  
-    afterAll(() => {
-      pool.end();
-    }); 
+  beforeEach(() => {
+    return setup(pool);
+  });
 
-    it('should be able to create a publisher', async () => {
-        const res = await request(app).post('/api/v1/publishers').send({name: 'Random House', city: 'New York City', state: 'New York', country: 'USA'})
-        expect(res.body).toEqual({
-            id: expect.any(String),
-            name: 'Random House', 
-            city: 'New York City', 
-            state: 'New York', 
-            country: 'USA',
-        })
-    })
+  afterAll(() => {
+    pool.end();
+  });
+
+  it('should be able to create a publisher', async () => {
+    const res = await request(app).post('/api/v1/publishers').send({
+      name: 'Random House',
+      city: 'New York City',
+      state: 'New York',
+      country: 'USA',
+    });
+    expect(res.body).toEqual({
+      id: expect.any(String),
+      name: 'Random House',
+      city: 'New York City',
+      state: 'New York',
+      country: 'USA',
+    });
+  });
+
+  it('should be able to get all publishers', async () => {
+    const expected = await Publisher.findAllPublisher();
+    const res = await request(app).get('/api/v1/publishers');
+
+    expect(res.body).toEqual(expected);
+  });
 });
