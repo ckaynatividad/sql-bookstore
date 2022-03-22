0e0b2a7ae832585b814c46ea7491e068f11d68fd
Emma Egstad <emma@egstad.com>
2022-03-22 16:22:44 -0500

    passing test for find all authors

--- __tests__/authors.test.js
+++ __tests__/authors.test.js
@@ -6,6 +6,7 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
+const Author = require('../lib/models/Author');
 
 describe('cat routes', () => {
   beforeEach(() => {
@@ -25,5 +26,12 @@ describe('cat routes', () => {
     const res = await request(app).post('/api/v1/authors').send(expected);
 
     expect(res.body).toEqual({ id: expect.any(String), ...expected });
+  });
+
+  it('gets a list of authors', async () => {
+    const expected = await Author.findAllAuthors();
+    const res = await request(app).get('/api/v1/authors');
+
+    expect(res.body).toEqual(expected);
   });
 });
