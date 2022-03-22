8ba338d853595d7d6e4535758759f86ed65b368d
ckaynatividad <natividad.ckay@gmail.com>
2022-03-22 11:17:29 -0700

    made junction tables and inserted data

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,6 +1,6 @@
 -- Use this file to define your SQL tables
 -- The SQL in this file will be executed when you run `npm run setup-db`
-DROP TABLE IF EXISTS authors, books, publishers, reviews, reviewers;
+DROP TABLE IF EXISTS authors, books, publishers, reviews, reviewers, authors_books CASCADE;
 
 CREATE TABLE publishers (
     id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
@@ -20,7 +20,7 @@ CREATE TABLE authors (
 CREATE TABLE books (
     id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     title TEXT NOT NULL,
-    publisher BIGINT NOT NULL,
+    publisher BIGINT REFERENCES publishers(id),
     released float(4) NOT NULL
 );
 
@@ -38,5 +38,25 @@ CREATE TABLE reviews (
     book BIGINT REFERENCES books(id)
 );
 
+CREATE TABLE authors_books (
+    books_id BIGINT REFERENCES books(id),
+    authors_id BIGINT REFERENCES authors(id)
+);
+
 INSERT INTO publishers (name, city, state, country)
 VALUES ('Penguin Publishers', 'New York City', 'New York', 'USA');
+
+INSERT INTO authors (name, dob, pob)
+VALUES ('Pinky Agnew', '01/01/1955', 'New Zealand');
+
+INSERT INTO books (title, publisher, released)
+VALUES ('Heartsongs: Readings for Weddings', '1', '2004');
+
+INSERT INTO reviewers (name, company)
+VALUES ('Amazon Customer', 'Amazon');
+
+INSERT INTO reviews (rating, reviewer, review, book)
+VALUES ('5', '1', 'Its impossible to read this book and not be moved. ', '1');
+
+INSERT INTO authors_books (books_id, authors_id)
+VALUES (1, 1);
