37a74e78a6483de55bd628a7581c3754b0f7c02c
ckaynatividad <natividad.ckay@gmail.com>
2022-03-23 16:05:54 -0700

    get books list passing

--- __tests__/books.test.js
+++ __tests__/books.test.js
@@ -2,6 +2,7 @@ const setup = require('../data/setup');
 const app = require('../lib/app');
 const request = require('supertest');
 const pool = require('../lib/utils/pool');
+const Book = require('../lib/models/Book');
 
 describe('sql - books routes', () => {
   beforeEach(() => {
@@ -20,5 +21,19 @@ describe('sql - books routes', () => {
     };
     const res = await request(app).post('/api/v1/books').send(expected);
     expect(res.body).toEqual({ id: expect.any(String), ...expected });
+  });
+
+  it('gets list of books', async () => {
+    const expected = [
+      {
+        title: 'Heartsongs: Readings for Weddings',
+        id: '1',
+        publisher: '1',
+        released: 2004,
+      },
+    ];
+    const res = await request(app).get('/api/v1/books');
+
+    expect(res.body).toEqual(expected);
   });
 });
